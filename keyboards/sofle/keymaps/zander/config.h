eb5fbc50dfceb4ed866a01ee3cd5c7e4cadd4aff
Zander Erasmus <zerasmus@corvusenergy.com>
2023-05-22 22:19:00 -0700

    Add custom keymap for sofle keyboard

---
+++ keyboards/sofle/keymaps/zander/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+//#define NO_DEBUG
+//#define NO_PRINT
+#define NO_ACTION_ONESHOT
+
