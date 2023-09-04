93e1a75bc0839ae42e50b18c9cb7136906918d34
Zander Erasmus <zerasmus@corvusenergy.com>
2023-09-04 16:41:15 -0700

    Modify keys for more ergonomics

--- keyboards/sofle/keymaps/zander/keymap.c
+++ keyboards/sofle/keymaps/zander/keymap.c
@@ -26,8 +26,12 @@ enum custom_keycodes {
     KC_VIMW,
     KC_VIME,
     KC_VIMS,
+    KC_VIMV,
     KC_VCPY,
-    KC_,
+    KC_LARR,
+    KC_RARR,
+    KC_LBIN,
+    KC_RBIN,
 };
 
 
@@ -50,29 +54,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [LAYER_BASE] = LAYOUT(
 KC_GRV,  KC_1, KC_2, KC_3, KC_4, KC_5,                   KC_6, KC_7, KC_8,  KC_9,  KC_0,   KC_MINS,
-KC_ESC,  KC_Q, KC_W, KC_E, KC_R, KC_T,                   KC_Y, KC_U, KC_I,  KC_O,  KC_P,   KC_BSPC, 
-KC_TAB,  KC_A, KC_S, KC_D, KC_F, KC_G,                   KC_H, KC_J, KC_K,  KC_L,  KC_SCLN,KC_QUOT, 
-KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MPLY, KC_MUTE, KC_N, KC_M, KC_COMM,KC_DOT,KC_SLSH,KC_DEL, 
+KC_ESC,  KC_Q, KC_W, KC_E, KC_R, KC_T,                   KC_Y, KC_U, KC_I,  KC_O,  KC_P,   KC_BSPC,
+KC_TAB,  KC_A, KC_S, KC_D, KC_F, KC_G,                   KC_H, KC_J, KC_K,  KC_L,  KC_SCLN,KC_QUOT,
+KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_MPLY, KC_MUTE, KC_N, KC_M, KC_COMM,KC_DOT,KC_SLSH,KC_DEL,
          KC_LGUI,KC_LALT,KC_LCTL,MO(1),KC_SPC,  KC_ENT, MO(2),KC_RSFT,KC_RALT,KC_RCTL),
 /* LOWER 1
  * ,-----------------------------------.                  ,-----------------------------------.
  * |PRNTS| F1  | F2  | F3  | F4  | F5  |                  | F6  | F7  | F8  | F9  | F10 | F11 |
  * |-----+-----+-----+-----+-----+-----|                  |-----+-----+-----+-----+-----+-----|
- * |  ▽  |  1  |  2  |  3  |  4  |  5  |                  |  6  |  7  |  8  |  9  |  0  |  ▽  |
+ * |  ▽  |  !  |  @  |  #  |  $  |  %  |                  |  ^  |  &  |  *  |  +  |  ▽  |  ▽  |
  * |-----+-----+-----+-----+-----+-----|                  |-----+-----+-----+-----+-----+-----|
  * |  ▽  |  ~  |  |  |  (  |  )  |  _  |-------.  ,-------|  ▶  |  [{ | }]  | - _ | = + | F12 |
  * |-----+-----+-----+-----+-----+-----|  PREV |  | NEXT  |-----+-----+-----+-----+-----+-----|
- * |  ▽  |  !  |  @  |  #  |  $  |  %  |-------|  |-------|  ^  |  &  |  *  | \ | |  ▽  |  ▽  |
+ * |  ▽  |  ▽  |  ▽  | <<  |  >> |  ▽  |-------|  |-------|  ▽  |  ▽  | <-  |  -> |  \  |  ▽  |
  * `-----------------------------------/       /   \      \-----------------------------------'
  *          |  ▽  |  ▽  |  ▽  |  ▽  | /   ▽   /     \  ▽   \ |  ▽  |  ▽  |  ▽  |  ▽  |
  *          |     |     |     |     |/       /       \      \|     |     |     |     |
  *          `-------------------------------'         '------'-----------------------'
  */
 [LAYER_LOWER] = LAYOUT(
-KC_PSCR,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,                      KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11, 
-_______,KC_1, KC_2, KC_3, KC_4, KC_5,                       KC_6, KC_7, KC_8, KC_9, KC_0, _______, 
-_______,KC_TILD,KC_PIPE,KC_LPRN,KC_RPRN,KC_UNDS,      KC_EPIPE,KC_LBRC,KC_RBRC,KC_MINS,KC_EQL,KC_F12, 
-_______,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,KC_MPRV,  KC_MNXT,KC_CIRC,KC_AMPR,KC_ASTR,KC_BSLS,_______,_______, 
+KC_PSCR,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,                      KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,
+_______,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,           KC_CIRC,KC_AMPR,KC_ASTR,KC_PPLS,_______, _______,
+_______,KC_TILD,KC_PIPE,KC_LPRN,KC_RPRN,KC_UNDS,      KC_EPIPE,KC_LBRC,KC_RBRC,KC_MINS,KC_EQL,KC_F12,
+_______,_______,_______,KC_LBIN,KC_RBIN,_______,KC_MPRV,  KC_MNXT,_______,_______,KC_LARR,KC_RARR,KC_BSLS,_______,
              _______,_______,_______,_______,_______,  _______,_______,_______,_______,_______),
 /* ARROWS 2
  * ,-----------------------------------.                  ,-----------------------------------.
@@ -82,17 +86,17 @@ _______,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,KC_MPRV,  KC_MNXT,KC_CIRC,KC_AMPR,K
  * |-----+-----+-----+-----+-----+-----|                  |-----+-----+-----+-----+-----+-----|
  * |  ▽  |     | :s/ |  {  |  }  |  %  |-------.  ,-------| PGD |  ←  |  ↓  |  →  |     |  ▽  |
  * |-----+-----+-----+-----+--=--+-----|  ▽    |  |    ▽  |-----+--=--+-----+-----+-----+-----|
- * |  ▽  |     |     |VCOPY|     |     |-------|  |-------|     |CTHom|     |CTEnd| Ins |  ▽  |
+ * |  ▽  |     |     |VCOPY| CR" |     |-------|  |-------|     |CTHom|     |CTEnd| Ins |  ▽  |
  * `-----------------------------------/       /   \      \-----------------------------------'
  *          |  ▽  |  ▽  |  ▽  |  ▽  | /   ▽   /     \  ▽   \ |  ▽  |  ▽  |  ▽  |  ▽  |
  *          |     |     |     |     |/       /       \      \|     |     |     |     |
  *          `-------------------------------'         '------'-----------------------'
  */
 [LAYER_ARROWS] = LAYOUT(
-KC_PSCR,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,                          KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11, 
+KC_PSCR,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,                          KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,
 _______,KC_VIMQ,KC_VIMW,KC_VIME,KC_APP,KC_PSCR,                 KC_PGUP,KC_HOME,KC_UP,KC_END,KC_NO,_______,
 _______,KC_NO,KC_VIMS,KC_LCBR,KC_RCBR,KC_PERC,            KC_PGDN,KC_LEFT,KC_DOWN,KC_RGHT,KC_NO,KC_F12,
-_______,XXXXXXX,XXXXXXX,KC_VCPY,XXXXXXX,KC_NO,_______,   _______,KC_NO,LCTL(KC_HOME),KC_NO,LCTL(KC_END),KC_INS,_______,
+_______,_______,_______,KC_VCPY,KC_VIMV,KC_NO,_______,   _______,KC_NO,LCTL(KC_HOME),KC_NO,LCTL(KC_END),KC_INS,_______,
              _______,_______,_______,_______,_______,   _______,_______,_______,_______,_______),
 /* MATH 3
  * ,-----------------------------------.                  ,-----------------------------------.
@@ -100,7 +104,7 @@ _______,XXXXXXX,XXXXXXX,KC_VCPY,XXXXXXX,KC_NO,_______,   _______,KC_NO,LCTL(KC_H
  * |-----+-----+-----+-----+-----+-----|                  |-----+-----+-----+-----+-----+-----|
  * |  ▽  |  [  |  ]  |  (  |  )  |  &  |                  |     |  7  |  8  |  9  |     |  ▽  |
  * |-----+-----+-----+-----+-----+-----|                  |-----+-----+-----+-----+-----+-----|
- * |  ▽  |  /  |  *  |  -  |  +  |  =  |-------.  ,-------|     |  4  |  5  |  6  |  0  |  ▽  |
+ * |  ▽  |  /  |  *  |  -  |  +  |  =  |-------.  ,-------|  .  |  4  |  5  |  6  |  0  |  ▽  |
  * |-----+-----+-----+-----+-----+-----|   ▽   |  |   ▽   |-----+-----+-----+-----+-----+-----|
  * |MO(4)|  %  |  ^  |  {  |  }  |  |  |-------|  |-------|     |  1  |  2  |  3  |  .  |  ▽  |
  * `-----------------------------------/       /   \      \-----------------------------------'
@@ -111,7 +115,7 @@ _______,XXXXXXX,XXXXXXX,KC_VCPY,XXXXXXX,KC_NO,_______,   _______,KC_NO,LCTL(KC_H
 [LAYER_MATH] = LAYOUT(
 KC_NO,_______,_______,_______,_______,_______,                  _______,_______,_______,_______,_______,_______,
 _______,KC_LBRC,KC_RBRC,KC_LPRN,KC_RPRN,KC_AMPR,                KC_NO,KC_P7,KC_P8,KC_P9,KC_NO,_______,
-_______,KC_PSLS,KC_PAST,KC_PMNS,KC_PPLS,KC_PEQL,                KC_NO,KC_P4,KC_P5,KC_P6,KC_P0,_______,
+_______,KC_PSLS,KC_PAST,KC_PMNS,KC_PPLS,KC_PEQL,              KC_PDOT,KC_P4,KC_P5,KC_P6,KC_P0,_______,
 MO(4),KC_PERC,KC_CIRC,KC_LCBR,KC_RCBR,KC_PIPE,_______,  _______,KC_NO,KC_P1,KC_P2,KC_P3,KC_PDOT,_______,
            _______,_______,_______,_______,_______,     _______,_______,_______,_______,_______),
 /* PROG 4
@@ -445,12 +449,44 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;
             }
         } break;
+        case KC_VIMV: {
+            if (record->event.pressed) {
+                register_code16(KC_LCTL);
+                SEND_STRING("r");
+                unregister_code16(KC_LCTL);
+                SEND_STRING("\"");
+                return false;
+            }
+        } break;
+        case KC_LARR: {
+            if (record->event.pressed) {
+                SEND_STRING("<-");
+                return false;
+            }
+        } break;
+        case KC_RARR: {
+            if (record->event.pressed) {
+                SEND_STRING("->");
+                return false;
+            }
+        } break;
+        case KC_LBIN: {
+            if (record->event.pressed) {
+                SEND_STRING("<<");
+                return false;
+            }
+        } break;
+        case KC_RBIN: {
+            if (record->event.pressed) {
+                SEND_STRING(">>");
+                return false;
+            }
+        } break;
     }
     return true;
 }
 
 #ifdef ENCODER_ENABLE
-
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
@@ -467,5 +503,5 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     }
     return true;
 }
-
 #endif
+
